c0737c1900cfce3af87439097c613cf43f9bb16a
andrey <laverentiy@gmail.com>
2026-03-06 16:10:37 +0300

    new tests

--- src/unit_tests/test_training.py
+++ src/unit_tests/test_training.py
@@ -18,22 +18,40 @@ class TestMultiModel(unittest.TestCase):
         self.multi_model = MultiModel()
 
     def test_log_reg(self):
-        self.assertEqual(self.multi_model.log_reg(), True)
+        """Тест обучения логистической регрессии"""
+        result = self.multi_model.log_reg()
+        self.assertEqual(result, True)
+        # Проверяем, что модель сохранена
+        self.assertTrue(os.path.isfile(self.multi_model.log_reg_path))
 
-    def test_rand_forest(self):
-        self.assertEqual(self.multi_model.rand_forest(use_config=False), True)
+    def test_model_accuracy(self):
+        """Тест что модель имеет приемлемую точность"""
+        self.multi_model.log_reg()
+        from sklearn.linear_model import LogisticRegression
+        import pickle
+        
+        # Загружаем обученную модель
+        with open(self.multi_model.log_reg_path, 'rb') as f:
+            classifier = pickle.load(f)
+        
+        # Проверяем точность на тестовом наборе
+        score = classifier.score(self.multi_model.X_test, self.multi_model.y_test)
+        self.assertGreater(score, 0.7)  # Минимум 70% accuracy
 
-    def test_knn(self):
-        self.assertEqual(self.multi_model.knn(use_config=False), True)
-
-    def test_svm(self):
-        self.assertEqual(self.multi_model.svm(use_config=False), True)
-
-    def test_gnb(self):
-        self.assertEqual(self.multi_model.gnb(), True)
-
-    def test_d_tree(self):
-        self.assertEqual(self.multi_model.d_tree(use_config=False), True)
+    def test_model_predictions(self):
+        """Тест что модель может делать предсказания"""
+        self.multi_model.log_reg()
+        from sklearn.linear_model import LogisticRegression
+        import pickle
+        
+        with open(self.multi_model.log_reg_path, 'rb') as f:
+            classifier = pickle.load(f)
+        
+        # Делаем предсказание на одном образце
+        sample = self.multi_model.X_test.iloc[:1]
+        prediction = classifier.predict(sample)
+        self.assertEqual(len(prediction), 1)
+        self.assertIn(prediction[0], ['M', 'R'])
 
 
 if __name__ == "__main__":
